adbf91ae9b610c68386a29349bbdb1a60b51963e
BaharKrc <144059055+BaharKrc@users.noreply.github.com>
2024-02-21 20:00:08 +0100

    add

---
+++ 01-oop1/oop1.js
@@ -0,0 +1,26 @@
+/* ------------------------------------------------------- */
+// const Car = {
+//     brand: 'Ford',
+//     model: 'Mustang',
+//     year: 1967,
+//     isAutoGear: true,
+//     colors: ['white', 'red'],
+//     details: {
+//         color1: 'White',
+//         color2: 'Red',
+//         engineSize: 4900
+//     },
+//     startEngine: function(param1){
+//         console.log(param1);
+//         return 'Engine runned'
+//     }
+// }
+// console.log(Car.brand);
+// console.log(Car.colors);
+// console.log(Car.Colors[0]);
+// console.log(Car.startEngine('value'));
+
+const testArray = ['value0', 'value1', 'value2', 'value3', 'value4']
+const var0 = testArray[0]
+const var1 = testArray[1]
+const var2 = testArray[2]
